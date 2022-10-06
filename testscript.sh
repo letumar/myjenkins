b1ce2eb39312086b5386bad80fd9e2101cacda67
SRLET <letumar@gmail.com>
2022-10-06 17:40:20 +0200

    Prac5

--- testscript.sh
+++ testscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
-echo "$NUMBER"
+echo "$NUMBER" is answer
 echo "This was added after webhook"
 
 echo "ola ice cream"
